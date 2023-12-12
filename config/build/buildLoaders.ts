1768982df1a5bf8c1d72fbc15da92435537b177c
ALEKSANDER KORKH <72800607+korkh@users.noreply.github.com>
2023-12-12 12:04:27 +0100

    add webpack config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,33 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.(sa|sc|c)ss$/,
+    use: [
+      // Creates `style` nodes from js strings
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        // Translates CSS into CommonJS
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+          },
+        },
+      },
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [typescriptLoader, cssLoader];
+}
