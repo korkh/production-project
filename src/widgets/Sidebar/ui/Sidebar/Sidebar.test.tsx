1cb3cee627b09e20c310c5df976df5b3918ba601
ALEKSANDER KORKH <72800607+korkh@users.noreply.github.com>
2023-12-30 21:13:40 +0100

    fixed test for Sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,20 +1,18 @@
 import { fireEvent, screen } from "@testing-library/react";
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
-import { Sidebar } from "./Sidebar";
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
+import { Sidebar } from "widgets/Sidebar/ui/Sidebar/Sidebar";
 
 describe("Sidebar", () => {
-  //--------- Render Component ---------------
-  test("Test render", () => {
-    renderWithTranslation(<Sidebar />);
+  test("with only first param", () => {
+    componentRender(<Sidebar />);
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
   });
-  // ---------- Toogle sidebar ----------------
+
   test("test toggle", () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId("sidebar-toggle");
     expect(screen.getByTestId("sidebar")).toBeInTheDocument();
     fireEvent.click(toggleBtn);
-    expect(screen.getByTestId("sidebar")).toHaveClass("collapsed"); 
-    // [cls.collapsed]: collapsed
+    expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
   });
 });
