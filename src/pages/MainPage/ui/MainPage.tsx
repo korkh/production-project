568e8fc4276380d6df686331d33615d1c90dbb5f
ALEKSANDER KORKH <72800607+korkh@users.noreply.github.com>
2024-01-03 12:17:42 +0100

    add redux store

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,17 +1,20 @@
 import { BugButton } from "app/providers/ErrorBoundary";
+import { Counter } from "entities/Counter";
 import { useTranslation } from "react-i18next";
 
 interface ErrorProps {
   errorInfo?: Error | null;
 }
 
-const MainPage = ({errorInfo}: ErrorProps) => {
+const MainPage = ({ errorInfo }: ErrorProps) => {
   const { t } = useTranslation();
 
   return (
     <div>
       <BugButton errorInfo={errorInfo} />
       {t("Main pagе")}
+      <br />
+      <Counter />
     </div>
   );
 };
