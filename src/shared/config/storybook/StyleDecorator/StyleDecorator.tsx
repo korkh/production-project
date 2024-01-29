ab6fb190ab252a2b94942ed20d31d77ff6cc0e1e
ALEKSANDER KORKH <72800607+korkh@users.noreply.github.com>
2024-01-29 16:20:45 +0100

    small updates

--- src/shared/config/storybook/StyleDecorator/StyleDecorator.tsx
+++ src/shared/config/storybook/StyleDecorator/StyleDecorator.tsx
@@ -4,6 +4,8 @@ interface Props {
   children?: React.ReactNode;
 }
 
-const StyleDecorator: FC<Props> = ({ children }) => <div style={{ padding: "20px" }}>{children}</div>;
+const StyleDecorator: FC<Props> = ({ children }) => (
+  <div style={{ padding: "20px" }}>{children}</div>
+);
 
 export default StyleDecorator;
