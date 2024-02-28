b6276cdc74f9085fdc250317d594eec756493d88
ALEKSANDER KORKH <72800607+korkh@users.noreply.github.com>
2024-02-28 14:07:22 +0100

    add html report for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -24,6 +24,18 @@ export default {
     "^axios$": "axios/dist/node/axios.cjs", // problem with import axios from "axios"
   },
   globals: { __IS_DEV__: true, __API__: "", __PROJECT__: "jest" },
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "<rootDir>/reports/unit",
+        filename: "report.html",
+        openReport: true,
+        inlineSource: true,
+      },
+    ],
+  ],
 };
 // Indicates whether the coverage information should be collected while executing the test
 // collectCoverage: false,
