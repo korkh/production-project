523fed5c50579f61d878ca5699eecf8ddee1e926
ALEKSANDER KORKH <72800607+korkh@users.noreply.github.com>
2024-03-13 14:06:38 +0100

    add theme addon to story book

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -4,10 +4,16 @@ const config: StorybookConfig = {
   stories: ["../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        backgrounds: false,
+      },
+    },
     "@storybook/addon-interactions",
     "@storybook/addon-docs",
     "storybook-addon-mock",
+    "storybook-addon-themes",
   ],
   framework: {
     name: "@storybook/react-webpack5",
--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -1,11 +1,9 @@
 import type { Preview } from "@storybook/react";
-import React from "react";
 
-import RouterDecorator from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
-import StyleDecorator from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
-import ThemeDecorator from "../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator";
 import "../../src/app/styles/index.scss";
 import { I18nDecorator } from "../../src/shared/config/storybook/i18nDecorator/i18nDecorator";
+import RouterDecorator from "../../src/shared/config/storybook/RouterDecorator/RouterDecorator";
+import StyleDecorator from "../../src/shared/config/storybook/StyleDecorator/StyleDecorator";
 import { SuspenseDecorator } from "../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator";
 import { Theme } from "../../src/shared/const/Theme";
 
@@ -19,18 +17,26 @@ const preview: Preview = {
       },
     },
     layout: "fullscreen",
+    themes: {
+      default: "light",
+      list: [
+        { name: "light", class: ["app", Theme.LIGHT], color: "#ffffff" },
+        { name: "dark", class: ["app", Theme.DARK], color: "#000000" },
+        { name: "orange", class: ["app", Theme.ORANGE], color: "#ffb005" },
+      ],
+    },
   },
   decorators: [
     (Story) => (
       <RouterDecorator>
         <SuspenseDecorator>
-          <ThemeDecorator theme={Theme.LIGHT}>
-            <I18nDecorator>
-              <StyleDecorator>
-                <Story />
-              </StyleDecorator>
-            </I18nDecorator>
-          </ThemeDecorator>
+          {/* <ThemeDecorator theme={Theme.LIGHT}> */}
+          <I18nDecorator>
+            <StyleDecorator>
+              <Story />
+            </StyleDecorator>
+          </I18nDecorator>
+          {/* </ThemeDecorator> */}
         </SuspenseDecorator>
       </RouterDecorator>
     ),
